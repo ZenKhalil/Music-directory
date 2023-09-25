f2bbc29df0f54697aed6cf0e49e1245ab80c8eb6
ZenKhalil <feattzen@gmail.com>
2023-09-26 00:34:51 +0200

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,12 +1,10 @@
-'use strict';
-
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(path.join(dirname, '/../config/config.json'))[env];
 const db = {};
 
 let sequelize;
@@ -17,7 +15,7 @@ if (config.use_env_variable) {
 }
 
 fs
-  .readdirSync(__dirname)
+  .readdirSync(dirname)
   .filter(file => {
     return (
       file.indexOf('.') !== 0 &&
@@ -27,7 +25,7 @@ fs
     );
   })
   .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    const model = require(path.join(dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
