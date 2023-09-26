a7cad37ef3138138d8e9ddbd072977f2e6f15926
ZenKhalil <feattzen@gmail.com>
2023-09-26 23:32:41 +0200

    updated

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,28 @@
 import dotenv from 'dotenv';
+import fs from 'fs/promises';
+import mysql from 'mysql2';
+
 dotenv.config();
 
-export const development = {
-    username: process.env.DB_USERNAME || 'root_base',
-    password: process.env.DB_PASSWORD || 'qwaszx1212@',
-    database: process.env.DB_DATABASE || 'music_db',
-    host: process.env.DB_HOST || '127.0.0.1',
+const development = {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql'
 };
+
+// Add SSL certificate if provided in environment variables
+if (process.env.DB_CERT === 'true') {
+    development.dialectOptions = {
+        ssl: {
+            ca: await fs.readFile("./DigiCertGlobalRootCA.crt.pem", 'utf8')
+        }
+    };
+}
+
+// Create a connection to the database
+const dbConnection = mysql.createConnection(development);
+
+export default dbConnection;
