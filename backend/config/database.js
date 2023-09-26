c800c99d1a35dd2d1162a48e88935488e33bca17
ZenKhalil <feattzen@gmail.com>
2023-09-26 23:03:59 +0200

    updates

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,8 +2,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const development = {
-    username: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD || 'Kim2300@',
+    username: process.env.DB_USERNAME || 'root_base',
+    password: process.env.DB_PASSWORD || 'qwaszx1212@',
     database: process.env.DB_DATABASE || 'music_db',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'mysql'
